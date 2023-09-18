dbf0991f2ca262ebec5099c732599814d72b43f9
LFourOne <patotorres0710@gmail.com>
2023-09-18 07:08:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("page loaded...");
+
+let videoPrev = document.getElementById("video");
+function play(){
+    video.play();
+    video.muted();
+}
+function pause(){
+    video.pause();
+}
